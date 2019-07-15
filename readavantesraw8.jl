4a5a1e2e402fc95a2dd4199d8ca9705647b01ef1
padmer <33146394+padmer@users.noreply.github.com>
2019-07-15 11:24:47 +0200

    Update readavantesraw8.jl

--- readavantesraw8.jl
+++ readavantesraw8.jl
@@ -6,17 +6,21 @@ Crummy function to read in a binary avasoft raw8 file
 """
 function readavantesraw8(filename,len=329)
 con=open(filename,"r")
-    mark(con)
+#Skip software name
 skip(con,14)
+#Store DeviceID
 DeviceID=""
 for i in 1:9
 DeviceID=string(DeviceID,read(con,Char))
 end
+#Skip binary that I don't know the meaning of
 skip(con,305)
+#Start of Xaxis Data
 Xaxis=zeros(len)
 for i in 1:len
     Xaxis[i]=read(con,Float32)
 end
+#Start of Yaxis Data
 Yaxis=zeros(len)
 for i in 1:len
     Yaxis[i]=read(con,Float32)
